91816941f2da183cd618bc5fbe34992942464805
Boris Iván Cordero Beecher <boris.cordero.2001@gmail.com>
2025-05-26 01:16:32 -0400

    Revert "Creacion de venta-service"
    This reverts commit be33a786afa023ceddef3cc88559fb8f1d7da2ed.

--- pedido-service/src/main/java/com/ecomarket/pedido_service/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecomarket.pedido_service.repository;
-
-import com.ecomarket.pedido_service.model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
-}
