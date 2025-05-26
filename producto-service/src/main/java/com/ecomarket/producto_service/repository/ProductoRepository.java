94e72b6c901b08e3b2685194001cf6eee9b522f0
Boris Iván Cordero Beecher <boris.cordero.2001@gmail.com>
2025-05-26 11:10:34 -0400

    modificacion producto-service

---
+++ producto-service/src/main/java/com/ecomarket/producto_service/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.ecomarket.producto_service.repository;
+
+import com.ecomarket.producto_service.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+}
