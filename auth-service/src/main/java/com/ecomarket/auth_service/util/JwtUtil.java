a2f63d132c71d2c5b9a1810e0fd8420234a8d098
Boris Iván Cordero Beecher <boris.cordero.2001@gmail.com>
2025-06-20 13:31:13 -0400

    Creación servicio de autenticación para JasonWebToken

---
+++ auth-service/src/main/java/com/ecomarket/auth_service/util/JwtUtil.java
@@ -0,0 +1,40 @@
+package com.ecomarket.auth_service.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long EXPIRATION_TIME = 1000 * 60 * 60 * 5; // 5 horas
+
+    public String generateToken(String username, String rol) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("rol", rol)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
